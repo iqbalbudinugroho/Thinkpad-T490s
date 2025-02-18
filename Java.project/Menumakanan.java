79e88673738f18c66a2b91eb2ac00e5c09cb3282
iqbalbudinugroho <iqbalbudinugroho@gmail.com>
2025-02-18 09:55:56 +0000

    main

---
+++ Java.project/Menumakanan.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menumakanan {
+    
+    public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+        final ArrayList<Interger> pesanan = new ArrayList<>();
+        int pilihan;
+
+        do {
+            tampilkanMenu();
+            
+        }
+    } 
+    }
+}
